1524d9f612d94753f86e3a51a0603101ecc59a4c
Ashley Hindle <hey@ashleyhindle.com>
2016-02-04 19:06:19 +0000

    Come on nginx, be a team player

--- provisioner.sh
+++ provisioner.sh
@@ -4,16 +4,17 @@ apt-get -y install nginx
 rm /etc/nginx/sites-enabled/default
 
 # Having to escape bash variables isn't good enough.  Better way?
-cat << EOF > /etc/nginx/sites-enabled/fodor-example.conf
+cat << EOF > /etc/nginx/sites-enabled/fodor-example
 	server {
 		listen 80 default_server;
+                listen [::]:80 default_server ipv6only=on;
 		root /var/www/fodor-example/;
-		index index.html;
+		index index.html index.htm;
 
 		server_name _;
 
 		location / {
-			try_files \$uri \$uri/ =404;
+			try_files \$uri \$uri/ /index.html;
 		}
 	}
 EOF
