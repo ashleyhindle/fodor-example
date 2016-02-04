97bc9efd8622c384eb97bd8efea9ea21fed83f93
Ashley Hindle <hey@ashleyhindle.com>
2016-02-04 21:05:44 +0000

    PATH, not DIR

--- fodor/provisioner.sh
+++ fodor/provisioner.sh
@@ -5,6 +5,6 @@ rm /etc/nginx/sites-enabled/default
 
 # This is run from the repo root, so must include the fodor dir even though the provisioner is in it
 cp fodor/nginx/fodor-example /etc/nginx/sites-enabled/
-chown -R www-data:www-data $INSTALLDIR
-chmod -R g+wr $INSTALLDIR
+chown -R www-data:www-data $INSTALLPATH
+chmod -R g+wr $INSTALLPATH
 service nginx restart
