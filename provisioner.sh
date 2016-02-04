73d976d4f949a85eacc6ac9586311ab2fef6e53e
Ashley Hindle <hey@ashleyhindle.com>
2016-02-04 18:59:16 +0000

    Default nginx server, apt-get with -y

--- provisioner.sh
+++ provisioner.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-apt-get update
-apt-get install nginx
+apt-get -y update
+apt-get -y install nginx
 rm /etc/nginx/sites-enabled/default
 
 cat << EOF > /etc/nginx/sites-enabled/fodor-example.conf
 	server {
-		listen 80;
+		listen 80 default_server;
 		root /var/www/fodor-example/;
 		index index.html index.htm;
 
